64e03cfe41beeb511214f570e106ff5d9a9a2895
zainabki <zainabidrissi2015@.com>
2023-04-26 01:16:39 -0700

    updated

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,12 +1,10 @@
 #include "main.h"
-	#include <stdio.h>
-
-
-	/**
-	 * main - check the code
-	 *
-	 * Return: Always 0.
-	 */
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 	int main(void)
 	{
 	    int r;
